5ce44b75514020fb115a7d267f739478b0711054
worsleyj <josephworsley23@gmail.com>
2024-08-15 15:24:15 -0400

    convert project to object instead of singular instance

--- src/index.js
+++ src/index.js
@@ -11,19 +11,29 @@ const testItem = createTodoItem("Title", "Description", "8/15/24", "Low", "", fa
 console.log(testItem.title);
 
 
-// dom stuff here, move to dif module on completion
+// ~~~ dom stuff here, move to dif module on completion ~~~
 
 const projectList = document.querySelector(".project-list");
 
 
 // does this need to be an object?
-const defaultProject = document.createElement("ul");
-defaultProject.className = "default-project";
-projectList.append(defaultProject);
-const projectTitle = document.createElement("h1");
-projectTitle.textContent = "Default Project"
-defaultProject.append(projectTitle);
 
-const itemOne = document.createElement("li"); 
-itemOne.textContent = testItem.title;
-defaultProject.append(itemOne);
+function createProject(title) {
+    const project = document.createElement("ul");
+    project.className = "project";
+    projectList.append(project);
+
+    const projectTitle = document.createElement("h1");
+    projectTitle.textContent = title;
+
+    const toDoItem = document.createElement("li");
+    const itemTitle = document.createElement("h2");
+    const itemDueDate = document.createElement("div");
+    itemTitle.textContent = testItem.title;
+    itemDueDate.textContent = testItem.dueDate;
+    toDoItem.append(itemTitle, itemDueDate)
+
+    project.append(projectTitle, toDoItem);
+}
+
+const defaultProject = createProject("Default Project");
