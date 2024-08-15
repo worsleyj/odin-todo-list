28a7feac453738e27d2101a09dd90c75483f37bf
worsleyj <josephworsley23@gmail.com>
2024-08-15 15:01:57 -0400

    add default project

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ import "./styles.css";
 
 console.log("Test");
 
-function createTodoItem() {
-    let title = "Title";
-    let desc = "Description";
-    let dueDate = "8/15/24";
-    let priority = "Medium";
-    let notes = "";
-    let checked = false;
-
+function createTodoItem(title, desc, dueDate, priority, notes, checked) {
     return {title, desc, dueDate, priority, notes, checked}
 }
 
-const testItem = createTodoItem();
+const testItem = createTodoItem("Title", "Description", "8/15/24", "Low", "", false);
 console.log(testItem.title);
+
+
+// dom stuff here, move to dif module on completion
+
+const projectList = document.querySelector(".project-list");
+
+
+// does this need to be an object?
+const defaultProject = document.createElement("ul");
+defaultProject.className = "default-project";
+projectList.append(defaultProject);
+const projectTitle = document.createElement("h1");
+projectTitle.textContent = "Default Project"
+defaultProject.append(projectTitle);
+
+const itemOne = document.createElement("li"); 
+itemOne.textContent = testItem.title;
+defaultProject.append(itemOne);
