5c348408030ba35d6d558547ba23647cbb126a59
worsleyj <josephworsley23@gmail.com>
2024-08-15 15:46:31 -0400

    add project object

--- src/index.js
+++ src/index.js
@@ -8,32 +8,42 @@ function createTodoItem(title, desc, dueDate, priority, notes, checked) {
 }
 
 const testItem = createTodoItem("Title", "Description", "8/15/24", "Low", "", false);
+const testItemTwo = createTodoItem("Title2", "Description2", "9/5/24", "High", "", false);
 console.log(testItem.title);
 
+function createProject(title) {
+    const createdDate = "8/15/24";
+    const items = [];
+    return {title, createdDate, items};
+}
 
+const defaultProject = createProject("Default Project");
 // ~~~ dom stuff here, move to dif module on completion ~~~
 
 const projectList = document.querySelector(".project-list");
 
-
-// does this need to be an object?
-
-function createProject(title) {
+function displayProject(title, projectArray) {
     const project = document.createElement("ul");
     project.className = "project";
     projectList.append(project);
 
     const projectTitle = document.createElement("h1");
     projectTitle.textContent = title;
+    project.append(projectTitle);
 
-    const toDoItem = document.createElement("li");
-    const itemTitle = document.createElement("h2");
-    const itemDueDate = document.createElement("div");
-    itemTitle.textContent = testItem.title;
-    itemDueDate.textContent = testItem.dueDate;
-    toDoItem.append(itemTitle, itemDueDate)
-
-    project.append(projectTitle, toDoItem);
+    projectArray.forEach(toDoItem => {
+        const itemLine = document.createElement("li");
+        const itemTitle = document.createElement("h2");
+        const itemDueDate = document.createElement("div");
+        itemTitle.textContent = toDoItem.title;
+        itemDueDate.textContent = toDoItem.dueDate;
+        itemLine.append(itemTitle, itemDueDate);
+        project.append(itemLine);
+    });
 }
 
-const defaultProject = createProject("Default Project");
+const projectArray = [];
+defaultProject.items.push(testItem);
+defaultProject.items.push(testItemTwo);
+
+displayProject(defaultProject.title, defaultProject.items);
