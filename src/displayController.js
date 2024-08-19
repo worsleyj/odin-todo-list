605bb52b2b1460ab9f56711464c4f1e9c7dcb6dd
worsleyj <josephworsley23@gmail.com>
2024-08-19 18:22:40 -0400

    del button

--- src/displayController.js
+++ src/displayController.js
@@ -9,35 +9,56 @@ function displayProject(title, projectArray) {
     const projectTitle = document.createElement("h1");
     projectTitle.textContent = title;
     project.append(projectTitle);
-
-    projectArray.forEach(toDoItem => {
-        const itemLine = document.createElement("li");
-        let index = 0;
-        itemLine.addEventListener("click", () => {
-            displaySelectedItem(toDoItem);
-        })
-        const itemTitle = document.createElement("h2");
-        const itemDueDate = document.createElement("div");
-        itemTitle.textContent = toDoItem.title;
-        itemDueDate.textContent = toDoItem.dueDate;
-        itemLine.append(itemTitle, itemDueDate);
-        project.append(itemLine);
-    });
+        // let index = -1;
+        projectArray.forEach((toDoItem, index) => {
+            console.log(index);
+            
+            const itemLine = document.createElement("li");
+            itemLine.addEventListener("click", () => {
+                displaySelectedItem(toDoItem);
+            })
+            const itemTitle = document.createElement("h2");
+            const itemDueDate = document.createElement("div");
+            const deleteBtn = document.createElement("button");
+            itemTitle.textContent = toDoItem.title;
+            itemDueDate.textContent = toDoItem.dueDate;
+            deleteBtn.textContent = "X";
+            deleteBtn.className = "delete-btn";
+            index++;
+            deleteBtn.addEventListener("click", () => {
+                console.log("INDEX BUT IS: " + index);
+                project.textContent = "";
+                console.log("REMOVED: " + projectArray[index].title);
+                
+                projectArray.splice(index, 1);
+                displayProject(title, projectArray);
+            });
+    
+            itemLine.append(itemTitle, itemDueDate, deleteBtn);
+            project.append(itemLine);
+        });
+    // displayProjectItems(project, projectArray);
 }
+
+// function displayProjectItems(project, projectArray) {
+//     
+// }
 
 function displaySelectedItem(selectedItem) {
     selection.textContent = "";
     const itemContainer = document.createElement("div");
     const itemTitle = document.createElement("h1");
     const itemDesc = document.createElement("p");
-    const itemDueDate = document.createElement("p");
+    const itemDueDate = document.createElement("div");
+    const itemPriority = document.createElement("div");
 
     itemTitle.textContent = selectedItem.title;
     itemDesc.textContent = selectedItem.desc;
     itemDueDate.textContent = selectedItem.dueDate;
+    itemPriority.textContent = selectedItem.priority;
 
     selection.append(itemContainer);
-    itemContainer.append(itemTitle, itemDesc, itemDueDate);
+    itemContainer.append(itemTitle, itemDesc, itemDueDate, itemPriority);
 }
 
 export {displayProject, displaySelectedItem};
--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ function createTodoItem(title, desc, dueDate, priority, tags, complete) {
 
 const testItem = createTodoItem("Note One", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium " +
     "inventore aspernatur. Voluptas, quibusdam cum! Repellendus corrupti tempora reiciendis vel minima temporibus maiores "+ 
-    "dolorum doloribus ducimus quia? Est, cumque illum.", "8/15/24", "Low", "", false);
-const testItemTwo = createTodoItem("Note about Bread", "I freaking love bread man", "9/5/24", "High", "", false);
-
+    "dolorum doloribus ducimus quia? Est, cumque illum.", "8/15/24", "3", "", false);
+const testItemTwo = createTodoItem("Note about Bread", "I freaking love bread man", "9/5/24", "1", "", false);
+const testItemThree = createTodoItem("Top 10 Animals", "Number 9: Cow", "1/1/24", "2", "", false)
 function createProject(title) {
     const createdDate = "8/15/24";
     const items = [];
@@ -20,9 +20,10 @@ function createProject(title) {
 const defaultProject = createProject("Default Project");
 defaultProject.items.push(testItem);
 defaultProject.items.push(testItemTwo);
+defaultProject.items.push(testItemThree);
 
 
 // ~~~ dom stuff here, move to dif module on completion ~~~
 displayProject(defaultProject.title, defaultProject.items);
 
-displaySelectedItem(defaultProject.items[1]);
+displaySelectedItem(defaultProject.items[0]); // default displayed note
