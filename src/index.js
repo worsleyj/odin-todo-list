a626ff1ccc5f31ec234de8f13b3a85b1c5b35d4f
worsleyj <josephworsley23@gmail.com>
2024-08-21 16:57:31 -0400

    add prettier

--- src/index.js
+++ src/index.js
@@ -4,20 +4,38 @@ import { displayProjects } from "./sidebar";
 import { displaySelectedItem } from "./mainDisplay";
 
 function createNoteItem(title, desc, dueDate, priority, complete) {
-    return {title, desc, dueDate, priority, complete};
+  return { title, desc, dueDate, priority, complete };
 }
 
 function createProject(title) {
-    const createdDate = "8/15/24";
-    const items = [];
-    return {title, createdDate, items};
+  const createdDate = "8/15/24";
+  const items = [];
+  return { title, createdDate, items };
 }
 
-const testItem = createNoteItem("Note One", "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium " +
-    "inventore aspernatur. Voluptas, quibusdam cum! Repellendus corrupti tempora reiciendis vel minima temporibus maiores "+ 
-    "dolorum doloribus ducimus quia? Est, cumque illum.", "08/20/2024", "3", false);
-const testItemTwo = createNoteItem("Note about Bread", "I freaking love bread man", "09/05/2024", "1", false);
-const testItemThree = createNoteItem("Top 10 Animals", "Number 9: Cow", "01/01/2024", "2", false)
+const testItem = createNoteItem(
+  "Note One",
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste accusantium " +
+    "inventore aspernatur. Voluptas, quibusdam cum! Repellendus corrupti tempora reiciendis vel minima temporibus maiores " +
+    "dolorum doloribus ducimus quia? Est, cumque illum.",
+  "08/20/2024",
+  "3",
+  false
+);
+const testItemTwo = createNoteItem(
+  "Note about Bread",
+  "I freaking love bread man",
+  "09/05/2024",
+  "1",
+  false
+);
+const testItemThree = createNoteItem(
+  "Top 10 Animals",
+  "Number 9: Cow",
+  "01/01/2024",
+  "2",
+  false
+);
 
 const projects = [];
 const defaultProject = createProject("Default Project");
@@ -35,4 +53,4 @@ defaultProject2.items.push(testItemThree);
 displayProjects(projects);
 displaySelectedItem(defaultProject.items[0]); // default displayed note
 
-export {createNoteItem, createProject, projects};
+export { createNoteItem, createProject, projects };
